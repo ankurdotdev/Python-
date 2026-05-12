e4dfa117f7ac994794a82a7fe306571fc6e7a5e5
ankurdotdev <vermaankur0600@gmail.com>
2026-05-13 00:01:10 +0530

    missing value from o->n

--- leetcode/05_problem.py
+++ leetcode/05_problem.py
@@ -1,31 +1,33 @@
 # missing number using xor
 
 
-# def missingNumber(nums): 
-#     result = len(nums)
-#     for i in range(len(nums)):
-#         result^=i^nums[i]
-    
-#     return result    
-    
-
-# nums =[9,8,6,4,3,2,1,0,5]
-# print(missingNumber(nums))
-
 def missingNumber(nums): 
-    freq = {}
-    for i in range(0, len(nums)+1):
-        freq[i] = 0
-
-    for i in range(0, len(nums)):
-        freq[nums[i]] = 1
-
-    for k,v in freq.items():
-        if v == 0:
-            return k      
+    result = len(nums)
+    for i in range(len(nums)):
+        result^=i^nums[i]
+    
+    return result    
     
 
 nums =[9,8,6,4,3,2,1,0,5]
 print(missingNumber(nums))
+
+#  using dictionary 
+
+# def missingNumber(nums): 
+#     freq = {}
+#     for i in range(0, len(nums)+1):git 
+#         freq[i] = 0
+
+#     for i in range(0, len(nums)):
+#         freq[nums[i]] = 1
+
+#     for k,v in freq.items():
+#         if v == 0:
+#             return k      
+    
+
+# nums =[9,8,6,4,3,2,1,0,5]
+# print(missingNumber(nums))
 
        
